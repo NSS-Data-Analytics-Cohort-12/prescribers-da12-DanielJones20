a3a3e5922f4574d528636296c8464cbb6d375a40
DanielJones <daniel.j202yahoo.com>
2024-06-13 21:30:56 -0500

    question4

--- Script/prescribers_script.sql
+++ Script/prescribers_script.sql
@@ -1,39 +1,76 @@
 -- 1.  a. Which prescriber had the highest total number of claims (totaled over all drugs)? Report the npi and the total number of claims.
 
--- Select prescriber.npi, Count(total_claim_count_ge65) AS count_of_claim,nppes_provider_last_org_name,nppes_provider_first_name
+-- Select prescriber.npi, SUM(total_claim_count) AS count_of_claim,nppes_provider_last_org_name,nppes_provider_first_name
 -- From prescriber
--- Inner Join prescription
+-- LEFT Join prescription
 -- On prescriber.npi = prescription.npi
+-- Where total_claim_count is not null
 -- Group By prescriber.npi,nppes_provider_last_org_name,nppes_provider_first_name
 -- Order By count_of_claim DESC
 -- Limit 10;
---Answer 250
+
+--Answer 99707
 
 -- --     b. Repeat the above, but this time report the nppes_provider_first_name, nppes_provider_last_org_name,  specialty_description, and the total number of claims.
--- Select prescriber.npi, Count(total_claim_count_ge65) AS count_of_claim,nppes_provider_last_org_name,nppes_provider_first_name, specialty_description
+-- Select prescriber.npi, SUM(total_claim_count) AS count_of_claim,nppes_provider_last_org_name,nppes_provider_first_name, specialty_description
 -- From prescriber
--- Inner Join prescription
+-- LEFT Join prescription
 -- On prescriber.npi = prescription.npi
+-- Where total_claim_count is not null
 -- Group By prescriber.npi,nppes_provider_last_org_name,nppes_provider_first_name, specialty_description
 -- Order By count_of_claim DESC
 -- Limit 10;
 
+--Answer: Bruce Pendley
+
 -- 2.  a. Which specialty had the most total number of claims (totaled over all drugs)?
-Select specialty_description,total_claim_count
-From prescriber
-Inner Join prescription
-On prescriber.npi = prescription.npi
-Order By total_claim_count
-limit 15;
+-- Select specialty_description,SUM(total_claim_count) AS sum_of_claim_count
+-- From prescriber
+-- Inner Join prescription
+-- On prescriber.npi = prescription.npi
+-- Group By specialty_description
+-- Order By sum_of_claim_count DESC
+-- limit 15;
+
+-- Answer:Family Practice
+
 --     b. Which specialty had the most total number of claims for opioids?
+-- Select specialty_description,Sum(total_claim_count) AS sum_of_claim_count
+-- From prescriber 
+-- Inner Join prescription
+-- On prescriber.npi = prescription.npi
+-- Inner Join drug
+-- On prescription.drug_name = drug.drug_name
+-- Where opioid_drug_flag = 'Y'
+-- Group By specialty_description
+-- Order By sum_of_claim_count DESC
+
+--Answer: Nurse Practitioner - 900845
 
 --     c. **Challenge Question:** Are there any specialties that appear in the prescriber table that have no associated prescriptions in the prescription table?
 
 --     d. **Difficult Bonus:** *Do not attempt until you have solved all other problems!* For each specialty, report the percentage of total claims by that specialty which are for opioids. Which specialties have a high percentage of opioids?
 
 -- 3.  a. Which drug (generic_name) had the highest total drug cost?
+-- Select sum(total_drug_cost) As sum_of_drug_cost, generic_name
+-- From prescription
+-- Inner Join drug
+-- On prescription.drug_name = drug.drug_name
+-- Group By generic_name
+-- Order By sum_of_drug_cost DESC
+
+--Answer: Insulin Glargine,HUM.REC>ANLONG
 
 --     b. Which drug (generic_name) has the hightest total cost per day? **Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.**
+Select generic_name, round((total_drug_cost / total_day_supply),2) As cost_per_day
+From prescription 
+Inner Join drug
+On prescription.drug_name = drug.drug_name
+Group By generic_name, (total_drug_cost / total_day_supply)
+Order By cost_per_day DESC
+limit 15;
+
+--Answer Immun Glob G(IGG)/GLY/IGA OV50
 
 -- 4. 
 --     a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs. **Hint:** You may want to use a CASE expression for this. See https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-case/ 
